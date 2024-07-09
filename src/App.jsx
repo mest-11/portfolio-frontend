445d21cffc4f323cacccaa4b8a8dcc55e335e9e6
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-07-09 14:57:36 +0000

    frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
+
+function App() {
+
+const router = createBrowserRouter([
+  {
+    
+  }
+])
+  return (
+    <>
+      
+        
+    </>
+  )
+}
+
+export default App
