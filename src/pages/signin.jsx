0e8c89b46cc5550ccceed9b7fb9ad9a4abd7d853
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-07-11 09:50:32 +0000

    This is to create sign in page

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -2,7 +2,30 @@
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+  
+
+<div className="flex flex-col items-center">
+  <div id="login-register">
+
+    <div id="box">
+      <label for="sign-up-toggle">Sign up</label>
+      <input type="checkbox" id="sign-up-toggle"/>
+      <label for="login-toggle">Login</label>
+      <input type="checkbox" id="login-toggle"/>
+      <div id="login-panel">
+        <form action="POST">
+          <input type="text" placeholder="First Name "  className="outline-none"/ >
+          <input type="text" placeholder="Last Name"  className="outline-none"/>
+          <input type="email" placeholder="Email"  className="outline-none"/>
+          <input type="password" placeholder="Password"/>
+        </form>
+        <button ><p>Sign up</p><p>Login</p></button>
+      </div>
+    </div>
+  </div>
+</div>
+   
+  
   )
 }
 
