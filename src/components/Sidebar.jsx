7ba5f13b829191520698000b6c145bd7fbfc0b7b
ckeziahp <ckeziah95@gmail.com>
2024-07-16 09:17:03 +0000

    Added the section of dashboard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     return (
         <div className=" h-screen w-[300px] bg-portBlue shadow-2xl flex flex-col px-8 py-12">
             <div>   
-                <span className="text-3xl font-bold text-white text-center">Porto<b className='text-purple-600'>liooo</b></span>
+                <span className="text-3xl font-bold text-white text-center">Porto<b className='text-purple-600'>flio</b></span>
                 {/* <img src=" alt=" /> */}
             </div>
 
--- src/pages/dashboard/pages/AddProject.jsx
+++ src/pages/dashboard/pages/AddProject.jsx
@@ -2,7 +2,91 @@ import React from 'react'
 
 const AddProject = () => {
   return (
-    <div>AddProject</div>
+    <div><form action="">
+    <div className="border-blue-400 shadow-lg ">
+
+
+        <div>
+            <label htmlFor='Profile Picture'>Upload Image</label>
+            <input type='file' id='' />
+        </div>
+
+        <div>
+
+            <label htmlFor="location">Location</label>
+            <select className='' placeholder="" />
+            <option></option>
+            <option>Ghana</option>
+            <option>Nigeria</option>
+            <option>Canada</option>
+            <option>Germany</option>
+            <option>India</option>
+            <option>Norway</option>
+            <option>Germany</option>
+
+        </div>
+
+        <div>
+            <label htmlFor="location">Location</label>
+            <select className='' placeholder="" />
+            <option></option>
+            <option>Male</option>
+            <option>Female</option>
+        </div>
+
+        <div>
+            <label htmlFor="Gender">Gender</label>
+            <select className='' placeholder="" />
+            <option></option>
+            <option>Female</option>
+            <option>Male</option>
+        </div>
+
+        <div>
+            <label htmlFor="Bio">Bio</label>
+            <input type="text" className="" placeholder="" />
+        </div>
+
+        <div>
+            <label htmlFor='Contact'>Contact</label>
+            <input type='text' className='' />
+        </div>
+
+        <div>
+            <label htmlFor="About">About</label>
+            <input type="text" className="" placeholder="" />
+        </div>
+
+        <div>
+            <label htmlFor="DateofBirth">Date Of Birth</label>
+            <input type="date" className="" placeholder="" />
+        </div>
+
+        <div>
+            <label htmlFor='Language'>Languages</label>
+            <select className='' placeholder="" />
+            <option></option>
+            <option>English</option>
+            <option>French</option>
+            <option>Spanish</option>
+            <option>Italian</option>
+            <option>Chinese</option>
+            <option>Twi</option>
+            <option>Ga</option>
+            <option>Dagbani</option>
+            <option>Hausa</option>
+            <option>Yoruba</option>
+            <option>Bono</option>
+        </div>
+
+        <div>
+            <button className=""><span className="truncate">Sunbmit</span>
+            </button>
+        </div>
+    </div>
+
+</form>
+</div>
   )
 }
 
