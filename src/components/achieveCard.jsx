542fbe1d12a7c3c38e5176b8f4cb5f70e152fc28
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-07-18 09:09:20 +0000

    This is to add the achievement section

---
+++ src/components/achieveCard.jsx
@@ -0,0 +1,19 @@
+
+const AchieveCard = () => {
+    return (
+        <div> <div> <div className=" flex gap-x-10">
+            <span className="w-[300px]">2035 - Present</span>
+
+            <div className="flex flex-col">
+                <span className="font-medium text-[16px]">Organization</span>
+                <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.</p>
+            </div>
+
+
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default AchieveCard
