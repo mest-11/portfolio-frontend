3c3315a6bfad5f0ceee23150a144471e1924e39e
ckeziahp <ckeziah95@gmail.com>
2024-07-19 09:26:48 +0000

    Added the form to the Main folders

--- src/pages/dashboard/pages/AddExperience.jsx
+++ src/pages/dashboard/pages/AddExperience.jsx
@@ -6,23 +6,23 @@ const AddExperience = () => {
         <div className="layout-content-container flex flex-col w-[52px] py-2 max-w-[600px] flex-1 place-content-center border border-box border-50 border-blue-500 bg-inherit bg-blue-100">
         <h2>Share Your Experience</h2>
 
-        <form id="experienceForm">
-            <div class="form-group">
+        <form >
+            <div class="flex flex-col text-blue-600">
                 <label for="jobTitle">Job Title</label>
                 <input type="text" id="jobTitle" name="jobTitle" placeholder="" className='px-4 text-center pb-2 pt-1 focus:outline-0 focus:ring-0 border-[2px] rounded-md border-blue-500 h-7 m-2'/>
             </div>
 
-            <div class="form-group">
+            <div class="flex flex-col text-blue-600">
                 <label for="companyName">Company Name</label>
                 <input type="text" id="companyName" name="companyName" placeholder='Enter Here' className='px-4 text-center pb-2 pt-1 focus:outline-0 focus:ring-0 border-[2px] rounded-md border-blue-500 h-7 m-2'/>
             </div>
 
-            <div class="form-group">
+            <div class="flex flex-col text-blue-600">
                 <label for="location">Location</label>
                 <input type="text" id="location" name="location" placeholder="Enter Here" className='px-4 text-center pb-2 pt-1 focus:outline-0 focus:ring-0 border-[2px] rounded-md border-blue-500 h-7 m-2'/>
             </div>
 
-            <div class="form-group">
+            <div class="flex flex-col text-blue-600">
                 <label for="startDate">Start Date</label>
                 <input type="date" id="startDate" name="startDate" className='px-4 text-center pb-2 pt-1 focus:outline-0 focus:ring-0 border-[2px] rounded-md border-blue-500 h-7 m-2'/>
             </div>
@@ -32,12 +32,12 @@ const AddExperience = () => {
                 <input type="date" id="endDate" name="endDate" className='px-4 text-center pb-2 pt-1 focus:outline-0 focus:ring-0 border-[2px] rounded-md border-blue-500 h-7 m-2'/>
             </div>
 
-            <div class="">
+            <div class="flex flex-col text-blue-600">
                 <label for="description">Description</label>
                 <textarea id="description" name="description" placeholder='Enter Here' className='px-4 text-center pb-2 pt-1 focus:outline-0 focus:ring-0 border-[2px] rounded-md border-blue-500 h-7 m-2'/>
             </div>
 
-            <button type="submit">Submit Experience</button>
+            <button type="submit" className='animate-bounce rounded-md px-4 text-center pb-5 pt-1 focus:outline-0 focus:ring-0 border-[2px] border-blue-500 h-5 m-2 hover:bg-white'>Submit Experience</button>
 
         </form>
     </div>
